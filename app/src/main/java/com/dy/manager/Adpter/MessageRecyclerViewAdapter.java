480548aadc165a0d7f9762d015d52e82b598cae9
deng <978548481@qq.com>
2016-04-04 22:51:09 +0800

    任务网络下载

--- app/src/main/java/com/dy/manager/Adpter/MessageRecyclerViewAdapter.java
+++ app/src/main/java/com/dy/manager/Adpter/MessageRecyclerViewAdapter.java
@@ -1,6 +1,7 @@
 package com.dy.manager.Adpter;
 
-import android.support.v7.widget.CardView;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,11 +21,9 @@ public class MessageRecyclerViewAdapter extends RecyclerView.Adapter<MessageRecy
     List<Message> contents;
 
 
-
     public MessageRecyclerViewAdapter(List<Message> contents) {
         this.contents = contents;
     }
-
 
 
     @Override
@@ -49,8 +48,8 @@ public class MessageRecyclerViewAdapter extends RecyclerView.Adapter<MessageRecy
         Message message = contents.get(position);
         holder.titleTextView.setText(message.getMessageTitle());
         holder.contentTextView.setText(message.getMessageBody());
-        holder.countTextView.setText("更新"+message.getMessageCount()+"条");
-        holder.timeTextView.setText("更新时间："+message.getTime());
+        holder.countTextView.setText("更新" + message.getMessageCount() + "条");
+        holder.timeTextView.setText("更新时间：" + message.getTime());
 
     }
 
@@ -59,12 +58,37 @@ public class MessageRecyclerViewAdapter extends RecyclerView.Adapter<MessageRecy
         private TextView contentTextView;
         private TextView timeTextView;
         private TextView countTextView;
+
         public MessageViewHolder(View view) {
             super(view);
             titleTextView = (TextView) view.findViewById(R.id.tv_title);
             contentTextView = (TextView) view.findViewById(R.id.tv_content);
             countTextView = (TextView) view.findViewById(R.id.tv_count);
             timeTextView = (TextView) view.findViewById(R.id.tv_time);
+            view.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View view) {
+                    System.out.println(contents.get(getAdapterPosition() - 1).getMessageTitle());
+                    new AlertDialog.Builder(view.getContext()).setTitle("温馨提示")
+                            .setMessage("是否删除 " + contents.get(getAdapterPosition() - 1).getMessageTitle() + "?")
+                            .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+
+                                }
+                            })
+                            .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+
+                                }
+                            }).show();
+
+
+                    return true;
+                }
+            });
         }
+
     }
 }

a875ef5582bedea3383d4b1007ebe85e738af6ef
978548481@qq.com <d7577355>
2016-04-08 10:37:04 +0800

    nothing

---
+++ app/src/main/java/com/dy/manager/MyApplication.java
@@ -0,0 +1,13 @@
+package com.dy.manager;
+
+import android.app.Application;
+
+/**
+ * Created by deng on 16-4-8.
+ */
+public class MyApplication extends Application {
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
